eac31b85bbd405045a99fe52198f687535ae0f07
Facundo Ezequiel Villagra <ezefac.95@gmail.com>
2022-07-21 01:08:23 -0300

    firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyADGsQ01ZR-bf5y_NSNGMuZzakrTjhKrYI",
+  authDomain: "newecomm-ecd95.firebaseapp.com",
+  projectId: "newecomm-ecd95",
+  storageBucket: "newecomm-ecd95.appspot.com",
+  messagingSenderId: "791772358621",
+  appId: "1:791772358621:web:42d60ec4569a42f4307834"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
